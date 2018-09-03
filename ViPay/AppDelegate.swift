6690b5f641d895ad9242982d5f53d467140d2468
Samuel Rockson <you@yourdomain.com>
2018-09-03 10:58:55 +0800

    checking

--- ViPay/AppDelegate.swift
+++ ViPay/AppDelegate.swift
@@ -21,6 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        print("just checking")
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         if PFUser.current() == nil {
